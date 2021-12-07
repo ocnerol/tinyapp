f22b97db8f1cdd02f092505e13f310b9ab0fe870
Lorenço <lorenco.alvareznavarrete@gmail.com>
2021-12-07 02:38:52 +0000

    Set ejs as the view engine

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 8080; // default port is 8080
+const app = express();
+
+app.set('view engine', 'ejs');
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
