cad0c27b3664f6f5ce07dd7ab044a569b20ba4aa
Lorenço <lorenco.alvareznavarrete@gmail.com>
2021-12-07 15:28:02 +0000

    Add handler (in progress) for POST requests to /urls/new & design generateRandomString function

--- express_server.js
+++ express_server.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const app = express();
 const port = 8080; // default port is 8080
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function generateRandomString() {
+  const letters = [
+    'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h',
+    'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p',
+    'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x',
+    'Y', 'y', 'Z', 'z'
+  ];
+  const resultMaxLength = 6;
+  let result = "";
+  let addNumber = true;
+  for (let i = 0; i < resultMaxLength; i++) {
+    if (addNumber) {
+      const newNumber = Math.floor(Math.random() * 10); // random number (integer) from 0 to 9
+      result += newNumber;
+    } else {
+      const randomIndex = Math.floor(Math.random() * 53); // random number (integer) from 0 to 52 inclusive
+      const newLetter = letters[randomIndex];
+      result += newLetter;
+    }
+    addNumber = !addNumber;
+  }
+  return result;
+}
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
@@ -11,6 +37,10 @@ const urlDatabase = {
 
 app.get('/', (req, res) => {
   res.send('Hello!');
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
 app.get('/urls.json', (req, res) => {
@@ -26,8 +56,13 @@ app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
+app.post('/urls', (req, res) => {
+  console.log(req.body); // log the POST request body to the console
+  res.send('Ok')
+});
+
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
 });
 
