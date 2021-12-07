9376fc3923aae05180073cfd4b9e0e4ed17b6608
Lorenço <lorenco.alvareznavarrete@gmail.com>
2021-12-07 17:54:29 +0000

    Add redirect handler for short URLs to their long URL counterparts

--- express_server.js
+++ express_server.js
@@ -68,6 +68,11 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 })
