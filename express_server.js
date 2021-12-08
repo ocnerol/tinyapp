bdf5dfe7693d972ea272be04d69539ae81e661c4
Lorenço <lorenco.alvareznavarrete@gmail.com>
2021-12-08 04:03:13 +0000

    Display username in header and pass to all views

--- express_server.js
+++ express_server.js
@@ -49,7 +49,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  const templateVars = {
+    username: req.cookies["username"]
+  }
+  res.render('urls_new', templateVars);
 });
 
 app.get('/urls.json', (req, res) => {
@@ -57,7 +60,10 @@ app.get('/urls.json', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render('urls_index', templateVars);
 });
 
@@ -80,7 +86,11 @@ app.post('/urls', (req, res) => {
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
   const { shortURL } = templateVars;
   if (!shortURL || !urlDatabase[shortURL]) {
     res.sendStatus(404);
