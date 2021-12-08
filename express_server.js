3d8987270c6d57e85f3e293b268b96e1ce47b23f
Lorenço <lorenco.alvareznavarrete@gmail.com>
2021-12-08 03:22:34 +0000

    Integrate cookie parser

--- express_server.js
+++ express_server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8080; // default port is 8080
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 function generateRandomString() {
   const letters = [
