394a4118af642ad3d4955e4d64da5cc94a01e294
Lorenço <lorenco.alvareznavarrete@gmail.com>
2021-12-10 06:23:46 +0000

    Make generateRandomString function modular and revert to using let for for-loop counter since it is incremented iteratively

--- express_server.js
+++ express_server.js
@@ -7,7 +7,7 @@ const morgan = require('morgan');
 const bcrypt = require('bcryptjs');
 const salt = bcrypt.genSaltSync(10);
 const cookieSession = require('cookie-session');
-const { findUserByEmail } = require('./helpers');
+const { findUserByEmail, generateRandomString } = require('./helpers');
 
 
 app.use(cookieSession({
@@ -50,32 +50,6 @@ const users = {
 };
 
 // Functions
-
-const generateRandomString = function() {
-  const letters = [
-    'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h',
-    'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p',
-    'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x',
-    'Y', 'y', 'Z', 'z'
-  ];
-  const resultMaxLength = 6;
-  let result = "";
-  let addNumber = true;
-  for (const i = 0; i < resultMaxLength; i++) {
-    if (addNumber) {
-      const newNumber = Math.floor(Math.random() * 10); // random number (integer) from 0 to 9
-      result += newNumber;
-    } else {
-      const randomIndex = Math.floor(Math.random() * letters.length); // random number (integer) from 0 to length of letters array - 1
-      const newLetter = letters[randomIndex];
-      result += newLetter;
-    }
-    addNumber = !addNumber;
-  }
-  return result;
-};
-
-
 
 // returns URLs where the userID is equal to the given id (that of the currently logged-in user)
 const urlsForUser = function(id) {
--- helpers.js
+++ helpers.js
@@ -9,6 +9,31 @@ function findUserByEmail(email, database) {
   return undefined;
 };
 
+const generateRandomString = function() {
+  const letters = [
+    'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h',
+    'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p',
+    'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x',
+    'Y', 'y', 'Z', 'z'
+  ];
+  const resultMaxLength = 6;
+  let result = "";
+  let addNumber = true;
+  for (let i = 0; i < resultMaxLength; i++) {
+    if (addNumber) {
+      const newNumber = Math.floor(Math.random() * 10); // random number (integer) from 0 to 9
+      result += newNumber;
+    } else {
+      const randomIndex = Math.floor(Math.random() * letters.length); // random number (integer) from 0 to length of letters array - 1
+      const newLetter = letters[randomIndex];
+      result += newLetter;
+    }
+    addNumber = !addNumber;
+  }
+  return result;
+};
+
 module.exports = { 
-  findUserByEmail
+  findUserByEmail,
+  generateRandomString
 }
